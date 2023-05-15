0995ae4f351cb3925ce1a430fbb2c9ced25d644d
killingt1me <121635414+killaexist@users.noreply.github.com>
2023-05-15 07:04:34 +0300

    Update say_hello.py (test)

--- hello/scripts/say_hello.py
+++ hello/scripts/say_hello.py
@@ -1,3 +1,32 @@
+from brain_games.games.template import is_greeting, gcd_desc, wrong_answer
+import prompt
+from random import randint
+
+
+
+def is_gcd():
+    name = is_greeting()
+    gcd_desc()
+    count_correct = 0
+    while count_correct < 3:
+        r_num = randint(0, 99)
+        r_num2 = randint(0, 99)
+        print(f"Question: {r_num} {r_num2}")
+        ans = prompt.string("Your answer: ")
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
 from colorama import Fore
 
 
